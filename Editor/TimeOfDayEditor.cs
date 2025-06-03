14f162aa9e4867f619c6e55d47c8cacf106bf8d4
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-03 22:15:56 +0200

    ⠀⠀⠀⠀⠀

--- Editor/TimeOfDayEditor.cs
+++ Editor/TimeOfDayEditor.cs
@@ -15,12 +15,15 @@ namespace UnityEssentials
             if (prefab != null)
             {
                 var timeOfDay = prefab.GetComponent<TimeOfDay>();
-                timeOfDay.SunLight = prefab.transform.Find("Directional Sun Light")?.GetComponent<Light>();
-                timeOfDay.SunLightData = prefab.transform.Find("Directional Sun Light")?.GetComponent<HDAdditionalLightData>();
-                timeOfDay.MoonLight = prefab.transform.Find("Directional Moon Light")?.GetComponent<Light>();
-                timeOfDay.MoonLightData = prefab.transform.Find("Directional Moon Light")?.GetComponent<HDAdditionalLightData>();
-                timeOfDay.SkyVolume = prefab.transform.Find("Physical Based Sky Volume")?.GetComponent<Volume>();
-                timeOfDay.NightVolume = prefab.transform.Find("Night Color Adjustment Volume")?.GetComponent<Volume>();
+
+                timeOfDay.SunLight = prefab.transform.Find("Directional Sun Light").GetComponent<Light>();
+                timeOfDay.SunLightData = prefab.transform.Find("Directional Sun Light").GetComponent<HDAdditionalLightData>();
+
+                timeOfDay.MoonLight = prefab.transform.Find("Directional Moon Light").GetComponent<Light>();
+                timeOfDay.MoonLightData = prefab.transform.Find("Directional Moon Light").GetComponent<HDAdditionalLightData>();
+
+                timeOfDay.NightVolume = prefab.transform.Find("Night Color Adjustment Volume").GetComponent<Volume>();
+                timeOfDay.SkyVolume = prefab.transform.Find("Physical Based Sky Volume").GetComponent<Volume>();
                 if (timeOfDay.SkyVolume.profile.TryGet<PhysicallyBasedSky>(out var skyOverride))
                     timeOfDay.SkyMaterial = skyOverride.material.value;
             }
